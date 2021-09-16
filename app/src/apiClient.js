f12cb5d4a399de17f9aab5d84592ed204f25bd73
jenhuynh <jendoubleh@gmail.com>
2021-09-16 11:39:55 -0700

    Fix sighting route

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -1,19 +1,3 @@
-// export const getTasks = async () => {
-//   const response = await fetch("/api/tasks");
-//   return response.json();
-// };
-
-// export const addTask = async (name) => {
-//   const response = await fetch("/api/tasks", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ name }),
-//   });
-//   return response.json();
-// };
-
 //sighting function
 export const getSightings = async () => {
   const response = await fetch("/api/sightings");
