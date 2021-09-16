8c4bd525fe109970a849df80db05ac4f906136f9
jenhuynh <jendoubleh@gmail.com>
2021-09-16 11:43:15 -0700

    imported sighting router

--- server/server.mjs
+++ server/server.mjs
@@ -1,16 +1,12 @@
 import express from "express";
 import mime from "mime-types";
 
-import taskRouter from "./taskRouter.mjs";
-
+import sightingRouter from "./sightingRouter.mjs";
 const app = express();
-
-app.use("/api/tasks", taskRouter);
-
+app.use("/api/sightings", sightingRouter);
 app.get("/api/ping", (request, response) =>
   response.json({ response: "pong" }),
 );
-
 if (process.env?.SERVE_REACT?.toLowerCase() === "true") {
   app.use(
     express.static("/app", {
@@ -20,12 +16,10 @@ if (process.env?.SERVE_REACT?.toLowerCase() === "true") {
         res.setHeader("Cache-Control", "public, max-age=0"),
     }),
   );
-
   app.get("*", (req, res) => {
     res.sendFile("/app/index.html");
   });
 }
-
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.info(`Example server listening at http://localhost:${port}`);
