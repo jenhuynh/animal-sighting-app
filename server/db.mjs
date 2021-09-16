e1e6b8690c943c36f415813ae8be78b7783396c8
jenhuynh <jendoubleh@gmail.com>
2021-09-16 11:41:51 -0700

    Updated SQL queries

--- server/db.mjs
+++ server/db.mjs
@@ -3,10 +3,29 @@ import pgp from "pg-promise";
 
 const db = initDb();
 
-export const getTasks = () => db.any("SELECT * FROM tasks");
+// export const getTasks = async () => await db.any("SELECT * FROM tasks");
+//getting all information from table
+export const getSightings = async () =>
+  await db.any(
+    "SELECT sightings.individual_id, individuals.nickname, sightings.healthy, sightings.location, sightings.date, sightings.time, sightings.email FROM individuals JOIN sightings ON individuals.id=sightings.individual_id;",
+  );
 
-export const addTask = (name) =>
-  db.one("INSERT INTO tasks(name) VALUES(${name}) RETURNING *", { name });
+// export const addTask = (name) =>
+//   db.one("INSERT INTO tasks(name) VALUES(${name}) RETURNING *", { name });
+
+// animal is this object
+export const addSighting = async (sightings) =>
+  await db.one(
+    "INSERT INTO sightings(individual_id, location, healthy, email, date, time) VALUES ($1, $2, $3, $4, $5, $6)",
+    [
+      sightings["individual_id"],
+      sightings["location"],
+      sightings["healthy"],
+      sightings["email"],
+      sightings["date"],
+      sightings["time"],
+    ],
+  );
 
 function initDb() {
   let connection;
