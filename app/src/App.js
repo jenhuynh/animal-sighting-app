584bdea946f68708f58f60b86c30f0652a98942a
jenhuynh <jendoubleh@gmail.com>
2021-09-17 13:54:27 -0700

    added footer component

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,25 @@ import * as React from "react";
 
 import { Routes, Route, Link } from "react-router-dom";
 
+import Footer from "./Footer";
 import Sightings from "./Sightings";
 
 import "./App.css";
 
 const App = () => (
   <main>
+    <div className="hero-image">
+      <div className="hero-text">
+        <h1 className="hero-header">We Care</h1>
+        <p>An app that helps scientists and endangered species.</p>
+        <button className="hero">
+          <a href="#endangeredAnimals">View App</a>
+        </button>
+      </div>
+    </div>
     <nav>
-      <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
+      {/* <Link to="/">Home</Link> */}
+      {/* <Link to="dashboard">Dashboard</Link> */}
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
@@ -23,7 +34,9 @@ const Home = () => (
     <h1>{process.env.REACT_APP_TITLE}</h1>
     <h2>{process.env.REACT_APP_SUBTITLE}</h2>
     {/* <Tasks /> */}
+
     <Sightings />
+    <Footer />
   </>
 );
 
